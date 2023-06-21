03c04fd2dd56398c67955902fa80c89078edf6ce
Dennis Kimaita <dkimaita22@gmail.com>
2023-06-21 16:15:24 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    addToDo(document.getElementById('new-task-description').value)
+    form.reset()
+  })
+
 });
+function addToDo(todo){
+  let item = document.createElement('li')
+  item.textContent=`${todo}  `
+
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  btn.addEventListener('click', deletesTask)
+
+  item.appendChild(btn)
+  document.querySelector("#tasks").appendChild(item)
+
+}
+
+function deletesTask(event){
+  event.target.parentNode.remove()
+
+}
